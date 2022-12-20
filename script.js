31460438cbacede74e3949d9e6a304fb9c340f87
unknown <kcavner@hotmail.com>
2022-12-20 02:04:50 -0600

    bigchanges_bigchungus

--- script.js
+++ script.js
@@ -11,77 +11,83 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+  
   function generatePassword(){
     var characters = window.prompt("choose a length of at least 8 characters and no more than 128 characters for your password.");
-    // (characterArr.length = characters)
-    console.log(typeof characters)
+    var parsedCharacters = parseInt(characters)
+   
+   
+   if(Number.isNaN(parsedCharacters)){
+      window.confirm("please enter an integer between 8 and 128");
+      return;
+    }else if (parsedCharacters<8 || parsedCharacters>128){
+    window.confirm("please enter an integer between 8 and 128")
+    return;
+    }else{ (characterArr.length = parsedCharacters)}
+   
+   
+    
     var lowercaseConfirm = window.confirm("do you want to include lowercase letters?");
-    window.alert("confirmed lowercase")
-    var uppercaseConfirm = window.confirm("do you want to include Uppercase letters?");
-    window.alert("confirmed uppercase")
-    var numbersConfirm = window.confirm("do you want to include numbers?");
-    window.alert("confirmed numbers")
-    var specialConfirm = window.confirm("do you want to include special characters?");
-    window.alert("confirmed special characters")
-    
     if(lowercaseConfirm == true){
-      
       var lowercaseConfirmed = lowercase
-    
+      window.alert("confirmed lowercase... i respect that")
+    }else{
+      window.alert("no lowercase... ok then")
     }
+    var uppercaseConfirm = window.confirm("do you want to include Uppercase letters?");
     if(uppercaseConfirm == true){
-      
       var uppercaseConfirmed = uppercase
-    
+      window.alert("confirmed uppercase... big fan")
+    }else{
+      window.alert("no uppercase... excelent taste")
     }
+    var numbersConfirm = window.confirm("do you want to include numbers?");
     if(numbersConfirm == true){
-      
       var numbersConfirmed = numberCase
-    
+      window.alert("confirmed numbers... now we're cooking")
+    }else{
+      window.alert("no numbers... who needs 'em?")
     }
+    var specialConfirm = window.confirm("do you want to include special characters?");
     if(specialConfirm == true){
-      
       var specialConfirmed = specialCharacter
-      
+      window.alert("confirmed special characters... i love these little guys")
+    }else{
+      window.alert("no special characters... they're not that special anyways.")
     }
+    
+   
+    
+    
+    
     
     var concatCase = lowercaseConfirmed.concat(uppercaseConfirmed,numbersConfirmed,specialConfirmed)
-    console.log(concatCase)
+    
     
     let shuffled = concatCase
     .map(value => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value)
    
-    console.log(shuffled)
+    
     shuffled.length = +characters
-    console.log(shuffled)
+    
     var completeShuffled = shuffled.join("")
 
-    console.log(completeShuffled)
+    
     return completeShuffled
     
-    // c
-    // var shortenedCompleteShuffled = completeShuffled.
+    
     
   }
 }
  
-  // characterResponse = window.prompt("choose a length of at least 8 characters and no more than 128 characters for your password.");
-  // console.log(characterResponse)}
+
   
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// function generatePassword(){
-//   var password = window.prompt("choose a length of at least 8 characters and no more than 128 characters for your password.");
-//   console.log(password)
-  
-
-// }
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
