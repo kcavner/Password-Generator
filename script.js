12e72f7ebe2284921a03e96fbe5a6ac371b2fcd1
unknown <kcavner@hotmail.com>
2022-12-22 15:49:49 -0600

    madechancesequal

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //  initial variables include library 
   var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
   var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  var numberCase = ["1","2","3","4","5","6","7","8","9","0"]
-  var specialCharacter = ["!","@","#","$","%","^","&","*"]
+  var numberCase = ["1","2","3","4","5","6","7","8","9","0","2","3","4","5","6","7","8","9","0","2","3","4","5","6","7","8"]
+  var specialCharacter = ["!","@","#","$","%","^","&","*","@","#","$","%","^","&","*","@","#","$","%","^","&","*","@","#",]
   var characterConfirmArr = []
   
   // Assignment Code
